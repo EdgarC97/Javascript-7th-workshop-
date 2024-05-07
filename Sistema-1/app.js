eb44a6e02ff25034be478e21f74f534b838bf5b9
EdgarC97 <ecardona742@gmail.com>
2024-05-07 17:42:56 -0500

    work on final section

--- Sistema-1/app.js
+++ Sistema-1/app.js
@@ -1,135 +1,165 @@
 //Prototipos base -------------------------------------
 //Constructor de usuario
-function Usuario (nombre,email,puntosAcumulados,clave) {
-    //Propiedades
-    this.nombre = nombre;
-    this.email = email;
-    this.puntosAcumulados = puntosAcumulados;
-    this.clave = clave;
+function Usuario(nombre, email, puntosAcumulados, clave) {
+  //Propiedades
+  this.nombre = nombre;
+  this.email = email;
+  this.puntosAcumulados = puntosAcumulados;
+  this.clave = clave;
 }
 
 //Metodos de usuario
 Usuario.prototype.acumularPuntos = function () {
-     return console.log("Has acumulado" + this.puntosAcumulados);
-}
+  return console.log("Has acumulado" + this.puntosAcumulados);
+};
 Usuario.prototype.canjearPuntos = function () {
-    return console.log("Canjea puntos");
-}
+  return console.log("Canjea puntos");
+};
 
 //Constructor de administrador
-function Administrador (nombre,email,clave){
-    //Propiedades
-    this.nombre = nombre;
-    this.email = email;
-    this.clave = clave;
+function Administrador(nombre, email, clave) {
+  //Propiedades
+  this.nombre = nombre;
+  this.email = email;
+  this.clave = clave;
 }
 //Metodos de administrador
-Administrador.prototype.agregarProducto = function (){
-    return console.log("Agrega producto");
-}
-Administrador.prototype.modificarProducto = function (){
-    return console.log("Modifica producto");
-}
-Administrador.prototype.eliminarUsuario = function() {
-    return console.log("Elimina usuario");
-}
-
+Administrador.prototype.agregarProducto = function () {
+  return console.log("Agrega producto");
+};
+Administrador.prototype.modificarProducto = function () {
+  return console.log("Modifica producto");
+};
+Administrador.prototype.eliminarUsuario = function () {
+  return console.log("Elimina usuario");
+};
 
 // Constructor de Producto
-class Producto {
-    //El constructor es un metodo especial que se ejecuta en el momento de instanciar la clase
-    // Atributos
-    constructor (nombre,precio){
-        this.nombre = nombre;
-        this.precio = precio;
-        }
-
-    //Metodos de Producto
-    calcularImpuesto (){
-        return this.precio * 1.18 //Iva del 18%
-    }
+function Producto(
+  id,
+  nombre,
+  puntosNecesarios,
+  cantidadDisponible,
+  precio,
+  stock
+) {
+  this.id = id;
+  this.nombre = nombre;
+  this.puntosNecesarios = puntosNecesarios;
+  this.cantidadDisponible = cantidadDisponible;
+  this.precio = precio;
+  this.stock = stock;
 }
-//Falta definir prototipos derivados de producto
-//Falta crear las instancias de prototipos derivados
+//Metodos de producto
+Producto.prototype.actualizarId = function () {
+  return console.log("Se actualiza Id");
+};
+Producto.prototype.verPrecio = function () {
+  return console.log(`El precio es: ${this.precio}`);
+};
+
+Producto.prototype.obtenerInfo = function () {
+    return console.log("INFORMACIÓN");
+}
+
+// Crear una instancia de Producto
+const miProducto = new Producto(1, "Producto1", 100, 50, 200, 300);
+
+// Llamar a los métodos y atributos
+console.log(miProducto.id);  // Imprime: 1
+console.log(miProducto.nombre);  // Imprime: "Producto1"
+miProducto.actualizarId();  // Imprime: "Se actualiza Id"
+miProducto.verPrecio();  // Imprime: "El precio es: 200"
+miProducto.obtenerInfo();  // Imprime: "INFORMACIÓN"
+
+
 
 //Prototipos derivados----------------------------------
 //Constructor de Producto Fisico
-function ProductoFisico (nombre,puntosNecesarios,cantidadDisponible,precio,stock) {
-    //Propiedades
-    this.nombre = nombre;
-    this.puntosNecesarios = puntosNecesarios;
-    this.cantidadDisponible = cantidadDisponible;
-    this.precio = precio;
-    this.stock = stock;
+function ProductoFisico(
+  nombre,
+  puntosNecesarios,
+  cantidadDisponible,
+  precio,
+  stock
+) {
+  //Propiedades
+  this.nombre = nombre;
+  this.puntosNecesarios = puntosNecesarios;
+  this.cantidadDisponible = cantidadDisponible;
+  this.precio = precio;
+  this.stock = stock;
 }
 //Metodos de Productos fisicos
-ProductoFisico.prototype.actualizarStock = function() {
-    return console.log("Se actualiza stock");
-}
-ProductoFisico.prototype.obtenerInfo = function (){
-    return console.log("Se obtiene info");
-}
+ProductoFisico.prototype.actualizarStock = function () {
+  return console.log("Se actualiza stock");
+};
+ProductoFisico.prototype.obtenerInfo = function () {
+  return console.log("Se obtiene info");
+};
 ProductoFisico.prototype.enviarProducto = function () {
-    return console.log("Envia un producto");
-}
+  return console.log("Envia un producto");
+};
 
 //Constructor de Producto Digital
-function ProductoDigital (nombre,puntosNecesarios,url,cantidadDisponible,stock) {
-    //Propiedades
-    this.nombre = nombre;
-    this.puntosNecesarios = puntosNecesarios;
-    this.url = url;
-    this.cantidadDisponible = cantidadDisponible;
-    this.stock = stock;
+function ProductoDigital(
+  nombre,
+  puntosNecesarios,
+  url,
+  cantidadDisponible,
+  stock
+) {
+  //Propiedades
+  this.nombre = nombre;
+  this.puntosNecesarios = puntosNecesarios;
+  this.url = url;
+  this.cantidadDisponible = cantidadDisponible;
+  this.stock = stock;
 }
 //Metodos de Productos Digitales
 ProductoDigital.prototype.obtenerInfo = function () {
-    return console.log("Se obtiene info");
-}
+  return console.log("Se obtiene info");
+};
 ProductoDigital.prototype.descargar = function () {
-    return console.log("descarga");
-}
-ProductoDigital.prototype.obtenerProductEmail = function(){
-    return console.log("Obtiene email de producto");
-}
+  return console.log("descarga");
+};
+ProductoDigital.prototype.obtenerProductEmail = function () {
+  return console.log("Obtiene email de producto");
+};
 
 //Constructor de Actividad
-function Actividad (tipo,puntosOtorgados){
-    //Propiedades
-    this.tipo = tipo;
-    this.puntosOtorgados = puntosOtorgados;
+function Actividad(tipo, puntosOtorgados) {
+  //Propiedades
+  this.tipo = tipo;
+  this.puntosOtorgados = puntosOtorgados;
 }
 //Metodos de Actividad
-Actividad.prototype.completarActividad = function (){
-    return console.log("Completa actividad");
-}
+Actividad.prototype.completarActividad = function () {
+  return console.log("Completa actividad");
+};
 
 //Constructor de Categoria DelProducto
-function CategoriaDelProducto (nombre,descripcion){
-    //Propiedades
-    this.nombre = nombre;
-    this.descripcion = descripcion;
+function CategoriaDelProducto(nombre, descripcion) {
+  //Propiedades
+  this.nombre = nombre;
+  this.descripcion = descripcion;
 }
 //Metodos de Categoria Del Producto
-CategoriaDelProducto.prototype.listaProductos = function(){
-    return console.log("Lista productos");
-}
+CategoriaDelProducto.prototype.listaProductos = function () {
+  return console.log("Lista productos");
+};
 
 //Constructor de Orden De Canje
-function OrdenDeCanje (usuario,producto,fecha){
-    //Propiedades
-    CategoriaDelProducto.prototype.usuario = usuario;
-    this.producto = producto;
-    this.fecha = fecha;
+function OrdenDeCanje(usuario, producto, fecha) {
+  //Propiedades
+  CategoriaDelProducto.prototype.usuario = usuario;
+  this.producto = producto;
+  this.fecha = fecha;
 }
 //Metodos Orden De Canje
-OrdenDeCanje.prototype.confirmarOrden = function (){
-    return console.log("Confirma orden");
-}
-OrdenDeCanje.prototype.cancelarOrden = function(){
-    return console.log("Cancela orden");
-}
-
-
-
-
+OrdenDeCanje.prototype.confirmarOrden = function () {
+  return console.log("Confirma orden");
+};
+OrdenDeCanje.prototype.cancelarOrden = function () {
+  return console.log("Cancela orden");
+};
